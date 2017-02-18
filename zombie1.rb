9d9ffd2ce131e239cf81eeeec42530789a4bf6ef
Jorge Jimenez <stellajimenez@Apple-Mac.local>
2017-02-17 20:07:35 -0500

    solved number 2

---
+++ zombie1.rb
@@ -0,0 +1,5 @@
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "Tactical 10mm"]
+
+
+
+puts zombie_apocalypse_supplies.sort_by { |word| word.downcase }
